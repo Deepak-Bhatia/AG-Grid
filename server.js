ba5b0b072a61ed82974f337edfd348e27ece92e5
deepakk <imdeepakkbhatia@gmail.com>
2022-05-26 18:24:24 +0530

    Heroku configuration

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/AG-Grid'));
+//app.use(express.static('./dist/ag-grid-demo'));
+app.use(express.static(__dirname + '/dist/ag-grid-demo>'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/AG-Grid/'}),
+    //res.sendFile('index.html', {root: 'dist/ag-grid-demo'}),
+    res.sendFile(path.join(__dirname+'/dist/ag-grid-demo/index.html')),
 );
 
 // Start the app by listening on the default Heroku port
