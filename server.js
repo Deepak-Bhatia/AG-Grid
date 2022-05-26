2ff1510c95c32b8fee27449caafdb96e499cecea
deepakk <imdeepakkbhatia@gmail.com>
2022-05-26 19:19:08 +0530

    Heroku configuration

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-//app.use(express.static('./dist/ag-grid-demo'));
-app.use(express.static(__dirname + '/dist/ag-grid-demo>'));
+app.use(requireHTTPS);
+app.use(express.static('./dist/ag-grid-demo'));
 
 app.get('/*', (req, res) =>
-    //res.sendFile('index.html', {root: 'dist/ag-grid-demo'}),
-    res.sendFile(path.join(__dirname+'/dist/ag-grid-demo/index.html')),
+    res.sendFile('index.html', {root: 'dist/ag-grid-demo/'}),
 );
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
