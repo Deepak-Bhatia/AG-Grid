b901b028cb0a4e46ac7bf3fc70a9aaf7c4836bf1
Santosh Singh <itsmerockingagain@gmail.com>
2022-05-27 11:11:44 -0700

    Created form

---
+++ src/app/enums/form-controls.enum.ts
@@ -0,0 +1,15 @@
+export enum UpdateFormControlsEnum {
+  JOB_NUMBER = 'jobNumber',
+  PRIORITY = 'priority',
+  TYPE = 'type',
+  LINE = 'line',
+  MACHINE_CODE = 'machineCode',
+  MACHINE_NAME = 'machineName',
+  OCC_DATE = 'occDate',
+  OCC_TIME = 'occTime',
+  TR = 'tr',
+  JOB_DESC = 'jobDesc',
+  SHOP = 'shop',
+  JOB_INITIATED_BY = 'shopInitiatedBy',
+  USER_ID = 'userId'
+}
