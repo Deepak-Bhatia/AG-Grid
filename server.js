6e96552fbb3253c26979fb4e428689913678e982
Deepak-Bhatia <dkb.liverpool@gmail.com>
2022-08-26 22:42:17 +0530

    API call handled

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 function requireHTTPS(req, res, next) {
     // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
+    // if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    //     return res.redirect('https://' + req.get('host') + req.url);
+    // }
     next();
 }
 
 const express = require('express');
 const app = express();
 
-app.use(requireHTTPS);
+//app.use(requireHTTPS);
 app.use(express.static('./dist/ag-grid-demo'));
 
 app.get('/*', (req, res) =>
